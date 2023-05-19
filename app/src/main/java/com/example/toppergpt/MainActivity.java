60db20317dbbcd489d5c2b085786da7ed5be8d4c
adc77 <amandogra2016@gmail.com>
2023-05-19 23:50:13 +0530

    repomade

---
+++ app/src/main/java/com/example/toppergpt/MainActivity.java
@@ -0,0 +1,182 @@
+package com.example.toppergpt;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    TextView welcomeTextView;
+    EditText messageEditText;
+    ImageButton sendButton;
+    List<Message> messageList;
+    MessageAdapter messageAdapter;
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
+    OkHttpClient client = new OkHttpClient.Builder()
+            .readTimeout(35, TimeUnit.SECONDS)
+            .build();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_initial_screen);
+
+        Button chatButton = findViewById(R.id.chatButton);
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the chat layout
+                setContentView(R.layout.activity_main);
+                initializeChatLayout();
+            }
+        });
+    }
+
+    private void initializeChatLayout(){
+        messageList = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recycler_view);
+        welcomeTextView = findViewById(R.id.welcome_text);
+        messageEditText = findViewById(R.id.message_edit_text);
+        sendButton = findViewById(R.id.send_button);
+
+        // Setup recycler view
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
+
+        sendButton.setOnClickListener((v)-> {
+            String question = messageEditText.getText().toString().trim();
+            addToChat(question,Message.SENT_BY_ME);
+            messageEditText.setText("");
+            callAPI(question);
+            welcomeTextView.setVisibility(View.GONE);
+        });
+    }
+
+    void addToChat (String message, String sentBy) {
+        runOnUiThread( new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message, sentBy));
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+            }
+        });
+
+    }
+
+    void addResponse (String response){
+        messageList.remove(messageList.size()-1);
+        addToChat(response,Message.SENT_BY_GPT);
+    }
+
+    void callAPI(String question){
+        //okhttp
+        messageList.add(new Message("Thinking...", Message.SENT_BY_GPT));
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("model", "gpt-3.5-turbo");
+
+            JSONArray messageArr = new JSONArray();
+
+            //instruction to gpt        for role:System
+//            JSONObject instruction = new JSONObject();
+//            instruction.put("role", "system");
+//            instruction.put("content", "You are an expert and a helpful teacher. Please provide an easy explanation: ");
+//            messageArr.put(instruction);
+
+            //user question
+            JSONObject obj = new JSONObject();
+            obj.put("role","user");
+            obj.put("content", question);
+            messageArr.put(obj);
+
+            jsonBody.put("messages", messageArr);
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e); //e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/chat/completions")
+                .header("Authorization", "Bearer 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")
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                addResponse("Message Failed due to "+e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()){
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0)
+                                .getJSONObject("message")
+                                .getString("content");
+                        addResponse(result.trim());
+
+//                        //first choice from array
+//                        JSONObject choice = jsonArray.getJSONObject(0);
+//                        //message object
+//                        JSONObject messageObject = choice.getJSONObject("message");
+//                        //role & content from message object
+//                        String role = messageObject.getString("role");
+//                        String content = messageObject.getString("content");
+//
+//                        //check the role, to determine instruction or user response
+//                        if (role.equals("system")) {
+//                            addResponse(content.trim()); //handle explanation
+//                        } else if (role.equals("user")) {
+//                            addResponse(content.trim());
+//                        }
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }else {
+                    addResponse("Message Failed due to "+response.body().string());
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/toppergpt/Message.java
@@ -0,0 +1,30 @@
+package com.example.toppergpt;
+
+public class Message {
+    public static String SENT_BY_ME = "You";
+    public static String SENT_BY_GPT = "GPT";
+
+    String message;
+    String sentBy;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSentBy() {
+        return sentBy;
+    }
+
+    public void setSentBy(String sentBy) {
+        this.sentBy = sentBy;
+    }
+
+    public Message(String message, String sentBy) {
+        this.message = message;
+        this.sentBy = sentBy;
+    }
+}
